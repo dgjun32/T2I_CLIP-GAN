087b4697a62dfd87383f2e9843f63e269326bf84
Chan Woo Kim <chanwkim01@gmail.com>
2021-11-07 07:44:35 +0000

    clip finetuning GAN done

--- DMGAN+CLIP/code/datasets.py
+++ DMGAN+CLIP/code/datasets.py
@@ -25,8 +25,9 @@ else:
     import pickle
 
 
-def prepare_data(data, tokenizer):
-    imgs, text_captions, captions_lens, class_ids, keys, text_captions_2, captions_lens_2 = data
+def prepare_data(data):
+    imgs, captions, captions_lens, class_ids, keys, captions_2, captions_lens_2 = data
+
     # sort data by the length in a decreasing order
     sorted_cap_lens, sorted_cap_indices = \
         torch.sort(captions_lens, 0, True)
@@ -52,12 +53,10 @@ def prepare_data(data, tokenizer):
         else:
             real_imgs_2.append(Variable(imgs_2[i]))
 
-    captions = tokenizer.batch_encode_plus(text_captions, padding = True, return_tensors = 'pt')
-    captions = {'input_ids' : captions['input_ids'][sorted_cap_indices].squeeze().cuda(),
-                'attention_mask' : captions['attention_mask'][sorted_cap_indices].squeeze().cuda()}
-    captions_2 = tokenizer.batch_encode_plus(text_captions_2, padding = True, return_tensors = 'pt')
-    captions_2 = {'input_ids' : captions_2['input_ids'][sorted_cap_indices_2].squeeze().cuda(),
-                'attention_mask' : captions_2['attention_mask'][sorted_cap_indices_2].squeeze().cuda()}
+
+    captions = captions[sorted_cap_indices].squeeze()
+    
+    captions_2 = captions_2[sorted_cap_indices_2].squeeze()
     # sorted_captions_lens_2 = captions_lens_2[sorted_cap_indices].squeeze()
 
     # captions = torch.cat([captions, captions_2], dim=0)
@@ -73,7 +72,10 @@ def prepare_data(data, tokenizer):
     keys = [keys[i] for i in sorted_cap_indices.numpy()]
     # print('keys', type(keys), keys[-1])  # list
     if cfg.CUDA:
+        captions = Variable(captions).cuda()
         sorted_cap_lens = Variable(sorted_cap_lens).cuda()
+
+        captions_2 = Variable(captions_2).cuda()
         sorted_cap_lens_2 = Variable(sorted_cap_lens_2).cuda()
 
         sorted_cap_indices = sorted_cap_indices.cuda()
@@ -120,6 +122,283 @@ def get_imgs(img_path, imsize, bbox=None,
             ret.append(normalize(re_img))
 
     return ret
+
+
+
+class CLIPTextDataset(data.Dataset):
+    def __init__(self, data_dir, split='train',
+                 base_size=64,
+                 transform=None, target_transform=None, tokenizer=None):
+        self.tokenizer = tokenizer
+        print("TOKENIZER INSIDE DATASET", self.tokenizer)
+        assert self.tokenizer is not None
+
+
+        self.transform = transform
+        self.norm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.48145466, 0.4578275, 0.40821073),
+                                 std=(0.26862954, 0.26130258, 0.27577711))])
+
+        self.target_transform = target_transform
+        self.embeddings_num = cfg.TEXT.CAPTIONS_PER_IMAGE
+
+        self.imsize = []
+        for i in range(cfg.TREE.BRANCH_NUM):
+            self.imsize.append(base_size)
+            base_size = base_size * 2
+
+        self.data = []
+        self.data_dir = data_dir
+        if data_dir.find('birds') != -1:
+            self.bbox = self.load_bbox()
+        else:
+            self.bbox = None
+        split_dir = os.path.join(data_dir, split)
+
+        # if tokenizer:
+        self.filenames, self.captions = self.load_text_data(data_dir, split)
+        # else:
+            # self.filenames, self.captions, self.ixtoword, \
+            # self.wordtoix, self.n_words = self.load_text_data(data_dir, split)
+
+        self.class_id = self.load_class_id(split_dir, len(self.filenames))
+        self.number_example = len(self.filenames)
+        self.split = split
+
+    def load_bbox(self):
+        data_dir = self.data_dir
+        bbox_path = os.path.join(data_dir, 'CUB_200_2011/bounding_boxes.txt')
+        df_bounding_boxes = pd.read_csv(bbox_path,
+                                        delim_whitespace=True,
+                                        header=None).astype(int)
+        #
+        filepath = os.path.join(data_dir, 'CUB_200_2011/images.txt')
+        df_filenames = \
+            pd.read_csv(filepath, delim_whitespace=True, header=None)
+        filenames = df_filenames[1].tolist()
+        print('Total filenames: ', len(filenames), filenames[0])
+        #
+        filename_bbox = {img_file[:-4]: [] for img_file in filenames}
+        numImgs = len(filenames)
+        # for i in xrange(0, numImgs):
+        for i in range(0, numImgs):
+            # bbox = [x-left, y-top, width, height]
+            bbox = df_bounding_boxes.iloc[i][1:].tolist()
+
+            key = filenames[i][:-4]
+            filename_bbox[key] = bbox
+        #
+        return filename_bbox
+
+    def load_captions(self, data_dir, filenames):
+        all_captions = []
+        for i in range(len(filenames)):
+            cap_path = '%s/text/%s.txt' % (data_dir, filenames[i])
+            with open(cap_path, "r") as f:
+                captions = f.read().split('\n')
+                cnt = 0
+                for cap in captions:
+                    if len(cap) == 0:
+                        continue
+                    cap = cap.replace("\ufffd\ufffd", " ")
+                    # picks out sequences of alphanumeric characters as tokens
+                    # and drops everything else
+
+                    
+                    tokenizer = self.tokenizer
+                    tokens = tokenizer(cap)["input_ids"]
+                    
+                    all_captions.append(tokens)
+                    cnt += 1
+                    
+                    if cnt == self.embeddings_num:
+                        break
+
+                if cnt < self.embeddings_num:
+                    print('ERROR: the captions for %s less than %d'
+                          % (filenames[i], cnt))
+        return all_captions
+
+    def build_dictionary(self, train_captions, test_captions):
+        word_counts = defaultdict(float)
+        captions = train_captions + test_captions
+        for sent in captions:
+            for word in sent:
+                word_counts[word] += 1
+
+        vocab = [w for w in word_counts if word_counts[w] >= 0]
+
+        ixtoword = {}
+        ixtoword[0] = '<end>'
+        wordtoix = {}
+        wordtoix['<end>'] = 0
+        ix = 1
+        for w in vocab:
+            wordtoix[w] = ix
+            ixtoword[ix] = w
+            ix += 1
+
+        train_captions_new = []
+        for t in train_captions:
+            rev = []
+            for w in t:
+                if w in wordtoix:
+                    rev.append(wordtoix[w])
+            # rev.append(0)  # do not need '<end>' token
+            train_captions_new.append(rev)
+
+        test_captions_new = []
+        for t in test_captions:
+            rev = []
+            for w in t:
+                if w in wordtoix:
+                    rev.append(wordtoix[w])
+            # rev.append(0)  # do not need '<end>' token
+            test_captions_new.append(rev)
+
+        return [train_captions_new, test_captions_new,
+                ixtoword, wordtoix, len(ixtoword)]
+
+    def load_text_data(self, data_dir, split):
+        filepath = os.path.join(data_dir, 'captions.pickle')
+        train_names = self.load_filenames(data_dir, 'train')
+        test_names = self.load_filenames(data_dir, 'test')
+        if not os.path.isfile(filepath):
+            train_captions = self.load_captions(data_dir, train_names)
+            test_captions = self.load_captions(data_dir, test_names)
+
+            dump_content = [train_captions, test_captions]
+
+            if not (hasattr(self, "tokenizer") and self.tokenizer is not None):
+                train_captions, test_captions, ixtoword, wordtoix, n_words = \
+                    self.build_dictionary(train_captions, test_captions)
+                dump_content.append(ixtoword, wordtoix)
+                     
+            with open(filepath, 'wb') as f:
+                pickle.dump(dump_content, f, protocol=2)
+                print('Save to: ', filepath)
+        else:
+            with open(filepath, 'rb') as f:
+                x = pickle.load(f)
+                train_captions, test_captions = x[0], x[1]
+
+                if not (hasattr(self, "tokenizer") and self.tokenizer is not None):
+                    ixtoword, wordtoix = x[2], x[3]
+                    n_words = len(ixtoword)
+                del x
+                
+                print('Load from: ', filepath)
+        if split == 'train':
+            # a list of list: each list contains
+            # the indices of words in a sentence
+            captions = train_captions
+            filenames = train_names
+        else:  # split=='test'
+            captions = test_captions
+            filenames = test_names
+
+        if not (hasattr(self, "tokenizer") and self.tokenizer is not None):
+            return filenames, captions, ixtoword, wordtoix, n_words
+        else:
+            return filenames, captions
+
+    def load_class_id(self, data_dir, total_num):
+        if os.path.isfile(data_dir + '/class_info.pickle'):
+            with open(data_dir + '/class_info.pickle', 'rb') as f:
+                # class_id = pickle.load(f)
+                class_id = pickle.load(f, encoding='latin1')
+        else:
+            class_id = np.arange(total_num)
+        return class_id
+
+    def load_filenames(self, data_dir, split):
+        filepath = '%s/%s/filenames.pickle' % (data_dir, split)
+        if os.path.isfile(filepath):
+            with open(filepath, 'rb') as f:
+                filenames = pickle.load(f)
+            print('Load filenames from: %s (%d)' % (filepath, len(filenames)))
+        else:
+            filenames = []
+        return filenames
+
+    def get_caption(self, sent_ix):
+        # a list of indices for a sentence
+        sent_caption = np.asarray(self.captions[sent_ix]).astype('int64')
+        # if (sent_caption == 0).sum() > 0:
+        #     print('ERROR: do not need END (0) token', sent_caption)
+        
+        num_words = len(sent_caption)
+        # pad with 0s (i.e., '<end>')
+        x = np.zeros((cfg.TEXT.WORDS_NUM, 1), dtype='int64')
+        x_len = num_words
+        if num_words <= cfg.TEXT.WORDS_NUM:
+            x[:num_words, 0] = sent_caption
+        else:
+            ix = list(np.arange(num_words))  # 1, 2, 3,..., maxNum
+            np.random.shuffle(ix)
+            ix = ix[:cfg.TEXT.WORDS_NUM]
+            ix = np.sort(ix)
+            x[:, 0] = sent_caption[ix]
+            x_len = cfg.TEXT.WORDS_NUM
+        return x, x_len
+    def __getitem__(self, index):
+        #
+        key = self.filenames[index]
+        cls_id = self.class_id[index]
+        #
+        if self.bbox is not None:
+            bbox = self.bbox[key]
+            data_dir = '%s/CUB_200_2011' % self.data_dir
+        else:
+            bbox = None
+            data_dir = self.data_dir
+        #
+        if self.bbox is not None:
+            img_name = '%s/images/%s.jpg' % (data_dir, key)
+        elif data_dir.endswith('birds'):
+            img_name = '%s/CUB_200_2011/images/%s.jpg' % (data_dir, key)
+        elif data_dir.endswith('coco') and (self.split == 'train'):
+            img_name = '%s/train2014/%s.jpg' % (data_dir, key)
+        elif data_dir.endswith('coco') and (self.split == 'val'):
+            img_name = '%s/val2014/%s.jpg' % (data_dir, key)
+        imgs = get_imgs(img_name, self.imsize,
+                        bbox, self.transform, normalize=self.norm)
+        # random select a sentence
+        sent_ix = random.randint(0, self.embeddings_num)
+        new_sent_ix = index * self.embeddings_num + sent_ix
+        caps, cap_len = self.get_caption(new_sent_ix)
+        
+        # second sentence
+        sent_ix = random.randint(0, self.embeddings_num)
+        new_sent_ix = index * self.embeddings_num + sent_ix
+        caps_two, cap_len_two = self.get_caption(new_sent_ix)
+        
+        
+        return imgs, caps, cap_len, cls_id, key, caps_two, cap_len_two
+    def get_mis_caption(self, cls_id):
+        mis_match_captions_t = []
+        mis_match_captions = torch.zeros(99, cfg.TEXT.WORDS_NUM)
+        mis_match_captions_len = torch.zeros(99)
+        i = 0
+        while len(mis_match_captions_t) < 99:
+            idx = random.randint(0, self.number_example)
+            if cls_id == self.class_id[idx]:
+                continue
+            sent_ix = random.randint(0, self.embeddings_num)
+            new_sent_ix = idx * self.embeddings_num + sent_ix
+            caps_t, cap_len_t = self.get_caption(new_sent_ix)
+            mis_match_captions_t.append(torch.from_numpy(caps_t).squeeze())
+            mis_match_captions_len[i] = cap_len_t
+            i = i +1
+        sorted_cap_lens, sorted_cap_indices = torch.sort(mis_match_captions_len, 0, True)
+        #import ipdb
+        #ipdb.set_trace()
+        for i in range(99):
+            mis_match_captions[i,:] = mis_match_captions_t[sorted_cap_indices[i]]
+        return mis_match_captions.type(torch.LongTensor).cuda(), sorted_cap_lens.type(torch.LongTensor).cuda()
+    def __len__(self):
+        return len(self.filenames)
 
 
 class TextDataset(data.Dataset):
